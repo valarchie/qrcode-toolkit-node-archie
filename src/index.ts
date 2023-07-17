b7f088b9a0d217bb0436fa4fc0f3ff24b731ea83
valarchie <343928303@qq.com>
2023-07-17 21:58:31 +0800

    接收参数做成docker

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Router from "@koa/router";
 import { bodyParser } from "@koa/bodyparser";
 import { createCanvas } from "canvas";
 import { generateQRCode } from "./logic/generate";
+import { QRCodeGeneratorState } from "./logic/types";
 
 const app = new Koa();
 const router = new Router();
@@ -12,11 +13,13 @@ app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-router.get("/qrcode", async (ctx) => {
+router.post("/qrcode", async (ctx) => {
   const outCanvas = createCanvas(500, 500);
 
+  const params = ctx.request.body as QRCodeGeneratorState;
+
   // @ts-expect-error
-  await generateQRCode(outCanvas);
+  await generateQRCode(outCanvas, params);
 
   ctx.type = "image/png";
   ctx.body = outCanvas.toBuffer();
